9c2283d32adc9d16258aa368c28cf271f64744ff
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-14 17:48:39 +0900

    variables commit

---
+++ 1-index.php
@@ -0,0 +1,30 @@
+<?php
+
+$name = "Rob";
+
+echo $name;
+
+echo "<p>私の名前は、".$name."です。</p>";
+
+echo "<p>私の名前は${name}です。</p>";
+
+$myNumber = 45;
+
+echo "<p>番号：${myNumber}</p>";
+
+$calc = $myNumber * 31 / 97 + 4;
+
+echo "<p>計算結果：${calc}";
+
+//ブーリアン true, false
+
+$myBool = true;
+
+echo "<p>現在の状態は、${myBool}です。</p>";
+
+$myBool = false;
+
+//値が入っていないnullなので空白になる
+echo "<p>現在の状態は、${myBool}です。</p>";
+
+?>
