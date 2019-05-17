2e9b784fbe387e9e9e8933709b73cb92cda18ffa
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-17 17:03:04 +0900

    While Loops commit

--- 1-index.php
+++ 1-index.php
@@ -67,27 +67,39 @@
 //   echo $i."<br>";
 // }
 
-for($i = 2;$i <= 30;$i++){
-  echo $i.",";
+// for($i = 2;$i <= 30;$i++){
+//   echo $i.",";
+// }
+// echo "<br><br>";
+// for($i = 10;$i >= 0;$i--){
+//   if ($i == 0){
+//     echo $i;
+//   } else {
+//     echo $i.",";
+//   }
+// }
+// echo "<br><br>";
+// $family = array("Rob","Kirsten","Tommy","Ralphie");
+// for($i = 0; $i < sizeof($family);$i++){
+//   echo $family[$i].",";
+// }
+// echo "<br><br>";
+// foreach($family as $key => $value){
+//   // echo $key.":".$value.",";
+//   $family[$key] = $value." Percival";
+//   echo $family[$key].",";
+// }
+
+$i = 0;
+while($i < 10){
+  echo $i."<br>";
+  $i++;
 }
-echo "<br><br>";
-for($i = 10;$i >= 0;$i--){
-  if ($i == 0){
-    echo $i;
-  } else {
-    echo $i.",";
-  }
-}
-echo "<br><br>";
 $family = array("Rob","Kirsten","Tommy","Ralphie");
-for($i = 0; $i < sizeof($family);$i++){
-  echo $family[$i].",";
-}
-echo "<br><br>";
-foreach($family as $key => $value){
-  // echo $key.":".$value.",";
-  $family[$key] = $value." Percival";
-  echo $family[$key].",";
+$i = 0;
+while($i < sizeof($family)){
+  echo $family[$i]." Percival, ";
+  $i++;
 }
 
 ?>
